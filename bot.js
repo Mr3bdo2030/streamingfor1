a71930153dab8a13881f0340ed2342a930cbd8c6
Mr3bdo2030 <47391388+Mr3bdo2030@users.noreply.github.com>
2019-08-12 12:24:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "^";
-var adminprefix = '^'
+var prefix = "#";
+var adminprefix = '#'
 
 
 const developers = ["472795405940293652","424134723716120578"]
